b3001d8fbe0f71ab72ed8aec747138524b28e441
giskenf <giskefreberg@hotmail.com>
2021-01-28 15:51:01 +0100

    Task 2 done

--- core/src/giske/mygdx/game/states/PlayState.java
+++ core/src/giske/mygdx/game/states/PlayState.java
@@ -2,16 +2,20 @@ package giske.mygdx.game.states;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 import sprites.Heli;
 
 public class PlayState extends State {
     private Heli heli;
+    private BitmapFont font;
+
 
     public PlayState(GameStateManager gsm) {
         super(gsm);
         heli = new Heli(100,100);
+        font = new BitmapFont();
     }
 
     @Override
@@ -29,6 +33,7 @@ public class PlayState extends State {
     public void render(SpriteBatch sb) {
         sb.begin();
         sb.draw(heli.heliSprite, heli.getPosition().x, heli.getPosition().y);
+        font.draw(sb, heli.getPosition().toString() , 10, 780);
         sb.end();
     }
 
--- core/src/sprites/Heli.java
+++ core/src/sprites/Heli.java
@@ -3,8 +3,10 @@ package sprites;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
 import java.lang.Math;
 
@@ -23,6 +25,8 @@ public class Heli {
     public float inputPosX = 0;
     public float inputPosY = 0;
     private boolean touched = false;
+    private BitmapFont font;
+    private Rectangle rec;
 
     public Heli(int x, int y){
         direction = new Vector3(x,y,0);
@@ -37,9 +41,7 @@ public class Heli {
         if(touched){
             moveX(SPEED * dt);
             moveY(SPEED * dt);
-            //touchMove(SPEED * dt);
         }
-
     }
 
     public void handleInput(){
@@ -50,11 +52,14 @@ public class Heli {
             direction.x = Math.abs(inputPosX-position.x);
             direction.y = Math.abs(inputPosY-position.y);
             direction.nor();
+            rec = new Rectangle(inputPosX, inputPosY, heli.getWidth()/20, heli.getHeight()/20);
 
-            System.out.print(getPosition() + "\n");
+           /* System.out.print(getPosition() + "\n");
             System.out.print("X: " + inputPosX  + "\n");
             System.out.print("Y: " + inputPosY  + "\n");
-            System.out.print("direction: " + direction + "\n");
+            System.out.print("direction: " + direction + "\n");*/
+            System.out.print(heliSprite.getRegionX());
+
         }
     }
 
@@ -67,20 +72,24 @@ public class Heli {
     }
 
     public void moveX(float movement){
-        if(inputPosX > position.x){
+        if(inputPosX >= position.x){
             position.x += (movement * direction.x);
         }
         else{
             position.x -= (movement * direction.x);
         }
+        if(rec.contains(position.x, position.y)){
+            touched=false;
+        }
         if (position.x > MyGdxGame.WIDTH - heli.getWidth()) {
+            touched = false;
             heliSprite.flip(true, false);
-
         }
         if (position.x < 0) {
-            RIGHT = true;
+            touched = false;
             heliSprite.flip(true, false);
         }
+
     }
 
     public void moveY(float movement){
@@ -90,11 +99,16 @@ public class Heli {
         else{
             position.y -= (movement * direction.y);
         }
+        if(rec.contains(position.x, position.y)){
+            touched=false;
+        }
         if (position.y > MyGdxGame.HEIGHT - heli.getHeight()){
-            UP = false;
+            touched = false;
         }
         if (position.y <= 0){
-            UP = true;
+            touched = false;
         }
     }
+
+
 }
